a015cb03173fcb1ba73a52129ee2d541df1e1c32
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-09-25 14:55:29 -0600

    added more structures

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express')
-var app = express()
+var mongojs = require("mongojs");
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
 var exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
  
-mongoose.connect('mongodb://localhost/my_database');
+
 var bodyParser = require('body-parser')
 const cheerio = require('cheerio')
 const $ = cheerio.load('<h2 class="title">Hello world</h2>')
@@ -13,3 +20,15 @@ $('h2').addClass('welcome')
  
 $.html()
 var request = require('request');
+
+// Database configuration
+var databaseUrl = "scrapehome"
+var collections = ["scrapedData"]
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+mongoose.connect("mongodb://localhost/scraphomework", { useNewUrlParser: true });
