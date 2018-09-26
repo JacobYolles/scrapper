8327c80de97757c8186f168793260b6b93a15322
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-09-25 18:33:36 -0600

    added a new attempt

--- server.js
+++ server.js
@@ -27,13 +27,13 @@ db.on("error", function(error) {
 });
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost/scraphomework", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraphomework";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
-
+mongoose.connect("mongodb://localhost/scraphomework", { useNewUrlParser: true });
 
 // Create a generate all function
 // Retrieve data from the db
