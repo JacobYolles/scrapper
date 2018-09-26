fa22a9352517c37d6284d49b3eb0e6459add5a09
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-09-25 18:34:38 -0600

    added

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraphomework"
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
-mongoose.connect("mongodb://localhost/scraphomework", { useNewUrlParser: true });
+
 
 // Create a generate all function
 // Retrieve data from the db
