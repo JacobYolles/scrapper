e9596b48368b87439caadb53ed656884954466ae
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-09-27 09:37:30 -0600

    pruning files

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 // Retrieve data from the db
 app.get("/all", function(req, res) {
   // Find all results from the scrapedData collection in the db
-  db.scraphomework.find({}, function(error, found) {
+  db.Article.find({}, function(error, found) {
     // Throw any errors to the console
     if (error) {
       console.log(error);
@@ -95,6 +95,79 @@ app.get("/", function(req, res) {
       article: dbarticles
     });
   });
+});
+
+app.get("/articles", function(req, res) {
+
+  db.Article.find({}, function(error, found){
+    if (error) {
+      console.log(error)
+    }
+    else {
+      console.log(found)
+      res.json(found)
+    }
+  })
+
+
+  // TODO: Finish the route so it grabs all of the articles
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+
+  db.Article.findOne(
+    {
+    _id: req.params.id
+  })
+  .populate("note")
+  .then(function(dbArticle) {
+    res.json(dbArticle)
+  })
+  .catch(function(err) {
+    res.json(err)
+  })
+  
+  // TODO
+  // ====
+  // Finish the route so it finds one article using the req.params.id,
+  // and run the populate method with "note",
+  // then responds with the article with the note included
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+
+  db.Note.create(req.body)
+  .then(function (dbNote) {
+    // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+    // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+    // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+    // return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    return db.Article.findOneAndUpdate({
+      _id: req.params.id
+    }, {
+      
+        note: dbNote._id
+      
+    }, {
+      new: true
+    });
+  })
+  // GET RID OF THIS FOR IT TO STAY ON SCREEN
+  .then(function(dbArticle) {
+    // If we were able to successfully update an Article, send it back to the client
+    res.json(dbArticle);
+  })
+  .catch(function(err) {
+    // If an error occurred, send it to the client
+    res.json(err);
+  });
+  // TODO
+  // ====
+  // save the new note that gets posted to the Notes collection
+  // then find an article from the req.params.id
+  // and update it's "note" property with the _id of the new note
 });
 
 // Start the server
